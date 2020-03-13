fa97bfd7d4526c450545befbf19a27212c43a3b9
kuyaaaa <qq1034816916@163.com>
2020-03-13 17:32:00 +0800

    master

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 import router from './router'
-import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from './http'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
 
 new Vue({
   router,
